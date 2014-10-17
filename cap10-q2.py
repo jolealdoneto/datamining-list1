a90241fa6767daabb32e88665bfc01b8c0580d29
José Leal Domingues Neto <dominguesn@gmail.com>
2014-10-16 22:37:22 -0300

    feat(q2) Cap 10, q2

---
+++ cap10-q2.py
@@ -0,0 +1,45 @@
+
+seq_db = [ "ACGTCACG", "TCGA", "GACTGCA", "CAGTC", "AGCT", "TGCAGCTC", "AGTCAG" ]
+alf = [ "A", "C", "G", "T" ]
+
+def is_present(seq, phrase):
+    last = 0
+    for l in seq:
+        spot = phrase.find(l, last)
+        if spot == -1:
+            return False
+        last = spot+1
+    return True
+def sup(seq):
+    return sum(map(lambda s: 1 if is_present(seq, s) else 0, seq_db))
+
+def get_frequent(item, last, minsup):
+    freq_items = []
+    for l in last:
+        gen_item = item + l[-1]
+        if sup(gen_item) >= minsup:
+            freq_items.append(gen_item)
+    return freq_items + reduce(lambda acc, a: acc + a, map(lambda i: get_frequent(i, freq_items, 4), freq_items), [])
+
+# A
+all_frequent = get_frequent("", ["A", "C", "G", "T"], 4)
+print "frequent: ", all_frequent
+# B
+closed_freq = []
+for seq, i in zip(sorted(all_frequent, lambda a, b: len(a) - len(b)), range(len(all_frequent))):
+    closed = True
+    for pair in all_frequent[i+1:]:
+        if is_present(seq, pair) and sup(seq) == sup(pair):
+            closed = False
+            break
+    if closed:
+        closed_freq.append(seq)
+print "closed: ", closed_freq
+#C
+max_freq = []
+for seq, i in zip(all_frequent, range(len(all_frequent))):
+    if len(filter(lambda x: is_present(seq, x), all_frequent[i+1:])) == 0:
+        max_freq.append(seq)
+print "Max: ", max_freq
+
+
